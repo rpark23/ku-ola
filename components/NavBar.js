986edc459294be36a166380af54cfb3509f28837
Ryan Park <rpark@Ryans-MacBook-Pro-5.local>
2023-08-08 02:15:08 -1000

    announcements

--- components/NavBar.js
+++ components/NavBar.js
@@ -1,9 +1,22 @@
+import React, { useState, useEffect } from "react";
 import Image from 'next/image'
 import styles from '../styles/Home.module.css'
 
-export default function NavBar() {
+export default function NavBar(props) {
+  const { white } = props;
+
+  useEffect(() => {
+    console.log("white");
+    const navBar = document.getElementById("navbar");
+    if (white) {
+      navBar.style.backgroundColor = "white";
+    } else {
+      navBar.style.backgroundColor = "#faf2d9";
+    };
+  }, [white]);
+
   return (
-    <div className={styles.NavBar}>
+    <div className={styles.NavBar} id="navbar">
       <div className={styles.logo_container}>
         <Image src="/ku-ola-logo.png" layout="fill"/>
       </div>
